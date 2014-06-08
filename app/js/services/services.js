892e246c7f86af66abdb60b9889d740c4dc11a66
Alan Szpigiel <alanszp@gmail.com>
2014-06-08 19:42:47 -0300

    agrego servicio empanadas

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -1,32 +1,49 @@
-  app.factory('EmpanadasFactory', ["lodash", function(_){
-    var empanadas = [
-    {
-    	id: 1,
-    	nombre : 'Carne suave'
-    },
-    {
-    	id : 2,
-    	nombre : "Pollo"
-    },
-    {
-    	id : 3,
-    	nombre : "Humita"
-    },
-    {
-    	id : 4,
-    	nombre : "Carne cuchillo"
-    },
-    {
-    	id : 5,
-    	nombre : "Atun"
-    }];
+app.factory('EmpanadasService', ["lodash", function(_){
+  var empanadas = [
+  {
+    id: 1,
+    nombre : 'Carne suave'
+  },
+  {
+    id : 2,
+    nombre : "Pollo"
+  },
+  {
+    id : 3,
+    nombre : "Humita"
+  },
+  {
+    id : 4,
+    nombre : "Carne cuchillo"
+  },
+  {
+    id : 5,
+    nombre : "Atun"
+  }];
 
-   return {
-   		getEmpanadas: function () {
-        return empanadas
-      },
-   		getEmpanada: function (id) {
-   		   for(i;i)
-   		}
-   };
+ return {
+    getEmpanadas: function () {
+      return empanadas;
+    },
+    getEmpanada: function (id) {
+       _(empanadas).first(function (e) {
+          return e.id == id
+       });
+    },
+    add: function (nombre) {
+      var max = _(empanadas)
+      .map(function (e){
+        return e.id
+      })
+      .max();
+
+      var nueva = {
+        id: max + 1,
+        nombre: nombre
+      };
+
+      empanadas.push(nueva);
+      return nueva;
+    }
+  };
 }]);
